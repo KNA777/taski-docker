cf7f4987d27a287162f8d23411962254deb471de
Nikita <vostrotin.dan@yandex.ru>
2025-08-13 12:07:45 +0300

    first commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+    description = models.TextField(verbose_name='Описание')
+    completed = models.BooleanField(verbose_name='Завершен', default=False)
 
     def _str_(self):
         return self.title
